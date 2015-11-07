b49577c1a750ec248e1c9bb9ba1c51dcd7efc231
guwe <guwe@microsoft.com>
2015-11-07 17:48:33 +0000

    Add a template for user image

---
+++ my-101-vm-from-user-image/install-sth.sh
@@ -0,0 +1,11 @@
+
+
+echo `date` >> /tmp/install.log 2>&1
+echo "============================start======================================" >> /tmp/install.log 2>&1
+
+#yum -y update
+
+#yum install -y mongodb-org
+
+echo "============================end======================================" >> /tmp/install.log 2>&1
+
