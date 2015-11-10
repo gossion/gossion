99056dcf5afd1a9b83665235eba459fbbf6c0a3e
root <root@guwe-u1404.1uvuqkuziyne3lbjlje1pbgbhb.ix.internal.cloudapp.net>
2015-11-10 13:37:01 +0000

    record it on db

--- my-101-vm-from-user-image/mongo-install-centos.sh
+++ my-101-vm-from-user-image/mongo-install-centos.sh
@@ -5,6 +5,7 @@
 # Enable write access to the mongodb.repo and configure it for installation
 
 #sudo chmod 777 /etc/yum.repos.d/mongodb.repo
+start_at=`date | cut -d ' ' -f4`
 touch /etc/yum.repos.d/mongodb.repo
 echo "[mongodb-org-3.0]" >> /etc/yum.repos.d/mongodb.repo
 echo "name=MongoDB Repository" >> /etc/yum.repos.d/mongodb.repo
@@ -14,10 +15,21 @@ echo "enabled=1" >> /etc/yum.repos.d/mongodb.repo
 
 echo `date` >>/tmp/install.log
 echo "===========================startloging====================================" >> /tmp/install.log
-# Install updates
-yum -y update >> /tmp/install.log 2>&1
-
-echo "===========================midloging====================================" >> /tmp/install.log
 #Install Mongo DB
 yum install -y mongodb-org >>/tmp/install.log 2>&1
+status1=$?
+yum install -y gcc 
+status2=$?
+yum install -y gnome-desktop
+status3=$?
+echo "===========================midloging====================================" >> /tmp/install.log
+hostname=`hostname`
+status=$status1$status2$status3
+complete_at=`date | cut -d ' ' -f4`
+duration="${start_at}To${complete_at}"
+
+url="http://guwe-u1404.southeastasia.cloudapp.azure.com/stress/${hostname//-/_}/$status/${duration//:/_}/"
+echo "curl $url" >>/tmp/install.log 2>&1
+curl $url
+
 echo "===========================endloging====================================" >> /tmp/install.log
